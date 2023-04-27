c22f34336aa037c561415ca0adec3a23d9b25942
kevin-liu <liu.kevin1997@gmail.com>
2023-04-27 13:38:03 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React, { Component } from 'react';
-import { Question } from './components';
+import React, { Component } from "react";
+import { Question } from "./components";
 
-const category = '';
+const category = "";
 const TRIVIA_API = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=easy`;
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { question: null };
+	}
 
+	fetchTrivia() {
+		const res = fetch(TRIVIA_API);
+		const data = res.json();
+		this.setState({ question: data.results[0] });
+	}
+	componentDidMount() {
+		fetch(TRIVIA_API)
+			.then((res) => res.json())
+			.then((data) => this.setState({ question: data.results[0] }));
+	}
 
-  render() {
-    return (
-      <div className='container l:w-50 p-5'>
-        <h1 className='display-1'>Trivia</h1>
-        <h2 className='fw-lighter fs-5 mb-4'>
-          (we couldn&lsquo;t think of a better name,{' '}
-          <span className='fw-bolder'>sorry</span>)
-        </h2>
-        <hr />
-        <div>
-          {/* Render question here */}
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container l:w-50 p-5">
+				<h1 className="display-1">Trivia</h1>
+				<h2 className="fw-lighter fs-5 mb-4">
+					(we couldn&lsquo;t think of a better name,{" "}
+					<span className="fw-bolder">sorry</span>)
+				</h2>
+				<hr />
+				<div>
+					{this.state.question && <Question question={this.state.question} />}
+					{/* Render question here */}
+				</div>
+			</div>
+		);
+	}
 }
 
 export { App };
--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { decodeHTML } from '../lib';
+import React from "react";
+import { decodeHTML } from "../lib";
 
 class AnswerButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <button
-        onClick={this.props.handleGuess}
-        className='btn btn-outline-primary'
-      >
-        {decodeHTML(this.props.answer)}
-      </button>
-    );
-  }
+	render() {
+		return (
+			<button
+				onClick={() => this.props.handleGuess(this.props.answer)}
+				className="btn btn-outline-primary"
+			>
+				{decodeHTML(this.props.answer)}
+			</button>
+		);
+	}
 }
 
 export { AnswerButton };
